b79ae77e1e782cf2d6773dae33e32c84e238fae5
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-10-21 22:53:18 +0800

    feat(landing-page): add landing page component

---
+++ frontend/ibudget/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { LandingPage } from './landing-page/landing-page';
+
+@Component({
+  selector: 'app-root',
+  imports: [LandingPage],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('ibudget');
+}
