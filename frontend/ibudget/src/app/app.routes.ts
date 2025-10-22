26e96b04d372198f88f96a2cbd0db5061d5648f0
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2025-10-23 05:19:28 +0800

    feat(routing): add routing for several components
    add routing to:
     landing page > login page
     login-page > signup-page
     signup-page > login-page

--- frontend/ibudget/src/app/app.routes.ts
+++ frontend/ibudget/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
+import { Login } from './login/login';
 import { SignUp } from './sign-up/sign-up';
 import { EmailVerification } from './email-verification/email-verification';
 import { SetupAccount } from './setup-account/setup-account';
 
 export const routes: Routes = [
   {path: '', component: LandingPage},
+  {path: 'login-page', component: Login},
   {path: 'signup-page', component: SignUp},
   {path: 'email-verification', component: EmailVerification},
   {path: 'setup-account', component: SetupAccount}
