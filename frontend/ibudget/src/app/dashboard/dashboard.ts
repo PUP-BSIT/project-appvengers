511faaa6594367798df84381344959ee35d1d402
delima-justine <delimajustine24@gmail.com>
2025-10-29 22:10:22 +0800

    feat: add doughnut chart panel.

--- frontend/ibudget/src/app/dashboard/dashboard.ts
+++ frontend/ibudget/src/app/dashboard/dashboard.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
+import { DoughnutChart } from './doughnut-chart/doughnut-chart';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Sidebar],
+  imports: [Sidebar, DoughnutChart],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
-
+  
 }
