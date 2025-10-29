9667ddcfe7140769a116bba34d1a7835a16c70a7
delima-justine <delimajustine24@gmail.com>
2025-10-30 00:06:26 +0800

    feat: add categories component

---
+++ frontend/ibudget/src/app/categories/categories.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+
+@Component({
+  selector: 'app-categories',
+  imports: [Sidebar],
+  templateUrl: './categories.html',
+  styleUrl: './categories.scss',
+})
+export class Categories {
+
+}
