90acd8e6e50689eec9e1605870e3cf4114529dbf
delima-justine <delimajustine24@gmail.com>
2025-10-29 23:37:13 +0800

    feat: add history panel to dashboard.

--- frontend/ibudget/src/app/dashboard/dashboard.ts
+++ frontend/ibudget/src/app/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { DoughnutChart } from './doughnut-chart/doughnut-chart';
+import { HistoryTable } from "./history-table/history-table";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Sidebar, DoughnutChart],
+  imports: [Sidebar, DoughnutChart, HistoryTable],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
