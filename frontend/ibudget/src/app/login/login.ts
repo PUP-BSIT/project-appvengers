6bacdd5b718a4df0cf389dddcbc86d13028967c0
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-10-30 22:46:11 +0800

    feat(login): Update UI for login form.
    * feat(login): update placeholder texts for better UX
    
    * feat(login): add routerLink to dashboard on login button

--- frontend/ibudget/src/app/login/login.ts
+++ frontend/ibudget/src/app/login/login.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule, } from '@angular/common';
+import { Component, signal, inject  } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+    imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  loginForm: FormGroup;
+  hidePassword = signal(true);
+  submitted = signal(false);
 
+  fb = inject(FormBuilder);
+  router = inject(Router);
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', {
+        validators: [Validators.required, Validators.email]
+      }],
+      password: ['', {
+        validators: [Validators.required]
+      }]
+    });
+  }
+
+  onLogin(): void {
+    this.submitted.set(true);
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      // Navigate to dashboard or home page after successful login
+      this.router.navigate(['/dashboard']);
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
