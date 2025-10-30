df7934c672497e5543283bd2414eecf6f8a81ae9
delima_justine <delimajustine24@gmail.com>
2025-10-30 09:48:10 +0800

    feat: add core application pages (dashboard, transactions, budgets etc.)
    * feat: add dashboard component.
    
    * dependency: add chart.js
    
    * dependency: add ng2charts
    
    * feat: add doughnut chart component
    
    * config: add providerCharts().
    
    * feat: add doughnut chart panel.
    
    * feat: add history-table component
    
    * feat: add history panel to dashboard.
    
    * changes: remove extra space
    
    * feat: add budgets component.
    
    * fix: fix grammar of placeholder text.
    
    * feat: add transactions component
    
    * changes(routes): change default page to dashboard page temporarily and add dashboard, transactions, and budgets route.
    
    * feat(route): add temporary route of logout nav item to login page.
    
    * feat: add categories component
    
    * changes(routes): add categories route.
    
    * changes: remove comments.
    
    * changes(route): revert the default page to landing page.
    
    * fix: add RouterModule dependency
    
    * feat: add reports component.
    
    * feat: add notifications component.
    
    * feat: add settings component.
    
    * changes(routes): update routes for preparation of lazy loaded approach.
    
    * feat(route): add temporary route for login page that will direct to dashboard.

---
+++ frontend/ibudget/src/app/budgets/budgets.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+
+@Component({
+  selector: 'app-budgets',
+  imports: [Sidebar],
+  templateUrl: './budgets.html',
+  styleUrl: './budgets.scss',
+})
+export class Budgets {
+
+}
