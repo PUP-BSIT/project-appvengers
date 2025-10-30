8ac33cec13c72c3aac37266df00570645463e75b
delima_justine <delimajustine24@gmail.com>
2025-10-31 07:33:40 +0800

    feat: add app-header component and add it to each core components.
    * feat: add header component.
    
    * changes(dashboard): add app-header component.
    
    * changes(sidebar): update sidebar layout.
    
    * feat: add app-header component.
    
    * changes(header): update header layout.
    
    * changes(header): add route to notification icon.
    
    * changes(notifications): add app-header component.
    
    * changes(sidebar): remove comment on logout nav item.
    
    * chamges(header): add routes.

--- frontend/ibudget/src/app/categories/categories.ts
+++ frontend/ibudget/src/app/categories/categories.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
+import { Header } from "../header/header";
 
 @Component({
   selector: 'app-categories',
-  imports: [Sidebar],
+  imports: [Sidebar, Header],
   templateUrl: './categories.html',
   styleUrl: './categories.scss',
 })
