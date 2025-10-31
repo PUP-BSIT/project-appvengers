431e42b0f28d67e4f2b464d2f30963194a49066d
delima-justine <delimajustine24@gmail.com>
2025-10-31 18:33:16 +0800

    feat(categories): add categories panel to categories page.

--- frontend/ibudget/src/app/categories/categories.ts
+++ frontend/ibudget/src/app/categories/categories.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
+import { CategoriesPanel } from "./categories-panel/categories-panel";
 
 @Component({
   selector: 'app-categories',
-  imports: [Sidebar, Header],
+  imports: [Sidebar, Header, CategoriesPanel],
   templateUrl: './categories.html',
   styleUrl: './categories.scss',
 })
