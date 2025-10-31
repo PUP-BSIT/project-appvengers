da75dd06dd1ea6329cbba6d29f42d9188161c358
delima_justine <delimajustine24@gmail.com>
2025-10-31 12:51:30 +0800

    feat(budgets): add budgets page
    * feat: add initial budgets-panel
    
    * feat(budgets): update static budgets panel.

--- frontend/ibudget/src/app/budgets/budgets.ts
+++ frontend/ibudget/src/app/budgets/budgets.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
+import { BudgetsPanel } from "./budgets-panel/budgets-panel";
 
 @Component({
   selector: 'app-budgets',
-  imports: [Sidebar, Header],
+  imports: [Sidebar, Header, BudgetsPanel],
   templateUrl: './budgets.html',
   styleUrl: './budgets.scss',
 })
