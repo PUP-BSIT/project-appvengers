dce99fdd0eb87d8118e504dc0dfe9a67d761592e
delima-justine <delimajustine24@gmail.com>
2025-11-01 10:00:58 +0800

    changes(income service): add static income data.

--- frontend/ibudget/src/services/income.ts
+++ frontend/ibudget/src/services/income.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Income } from '../app/model/user_model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Income {
-  
+export class IncomeService {
+  private readonly STATIC_INCOME: Income[] = [
+    {name: 'Salary', amount: 3000, percentageCompleted: 75},
+    {name: 'Freelance', amount: 800, percentageCompleted: 50},
+    {name: 'Others', amount: 200, percentageCompleted: 20}
+  ]
+
+  getStaticIncome(): Income[] {
+    return [...this.STATIC_INCOME];
+  }
 }
