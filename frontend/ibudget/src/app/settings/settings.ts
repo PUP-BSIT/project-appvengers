b48da35357af07a18bc31eef9fdfe6e013cd2e5e
delima-justine <delimajustine24@gmail.com>
2025-11-01 20:47:57 +0800

    feat(settings): add personal information form.

--- frontend/ibudget/src/app/settings/settings.ts
+++ frontend/ibudget/src/app/settings/settings.ts
@@ -1,14 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
 import { SubHeader } from "../header/sub-header/sub-header";
+import { 
+    FormGroup, 
+    FormControl,
+    ReactiveFormsModule, 
+    FormBuilder,
+    Validators
+  } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
-  imports: [Sidebar, Header, SubHeader],
+  imports: [Sidebar, Header, SubHeader, ReactiveFormsModule],
   templateUrl: './settings.html',
   styleUrl: './settings.scss',
 })
 export class Settings {
+  accountSettingsForm: FormGroup;
+  formBuilder = inject(FormBuilder);
 
+  constructor() {
+    this.accountSettingsForm = this.formBuilder.group({
+      birthdate: ['', {
+        validators: [Validators.required]
+      }],
+      email: ['', {
+        validators: [Validators.required, Validators.email]
+      }],
+      first_name: ['', {
+        validators: [Validators.required]
+      }],
+      gender: ['', {
+        validators: [Validators.required]
+      }],
+      last_name: ['', {
+        validators: [Validators.required]
+      }],
+      middle_name: [''],
+      username: ['', {
+        validators: [Validators.required]
+      }],
+    })
+  }
+
+  get birthdate() {
+    return this.accountSettingsForm.get('birthdate');
+  }
+
+  get email() {
+    return this.accountSettingsForm.get('email');
+  }
+
+  get gender() {
+    return this.accountSettingsForm.get('gender');
+  }
+
+  get firstName() {
+    return this.accountSettingsForm.get('first_name');
+  }
+
+  get middleName() {
+    return this.accountSettingsForm.get('middle_name');
+  }
+
+  get lastName() {
+    return this.accountSettingsForm.get('last_name');
+  }
+
+  get username() {
+    return this.accountSettingsForm.get('username');
+  }
 }
