fbd2b0043be250d3d6ba56510c7e5071d7d10ec8
delima-justine <delimajustine24@gmail.com>
2025-11-01 15:20:43 +0800

    feat(settings): add sub-header to settings page.

--- frontend/ibudget/src/app/settings/settings.ts
+++ frontend/ibudget/src/app/settings/settings.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
+import { SubHeader } from "../header/sub-header/sub-header";
 
 @Component({
   selector: 'app-settings',
-  imports: [Sidebar, Header],
+  imports: [Sidebar, Header, SubHeader],
   templateUrl: './settings.html',
   styleUrl: './settings.scss',
 })
