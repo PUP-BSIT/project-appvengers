4a248ec6fdcf8b3e326885a50e177d1b8a898158
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-11-02 13:36:46 +0800

    fix(budget): rename user_model to user.model

--- frontend/ibudget/src/app/budgets/budgets-panel/budgets-panel.ts
+++ frontend/ibudget/src/app/budgets/budgets-panel/budgets-panel.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';  
+import { Expenses } from '../../model/user.model';
+import { ExpensesService } from '../../../services/expenses';
+import { Income } from '../../model/user.model';
+import { IncomeService } from '../../../services/income';
 
 @Component({
   selector: 'app-budgets-panel',
