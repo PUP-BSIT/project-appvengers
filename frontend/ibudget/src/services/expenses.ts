83dabc86b6817703e78947861ea9b0a0ccd94af6
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-11-02 13:37:18 +0800

    fix(expenses): rename user_model to user.model

---
+++ frontend/ibudget/src/services/expenses.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Expenses } from '../app/model/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpensesService {
+  private readonly STATIC_EXPENSES: Expenses[] = [
+    { 
+      name: 'Food', 
+      currentAmount: 350, 
+      allocatedAmount: 500, 
+      percentageUsed: 60 
+    },
+    { 
+      name: 'Transportation', 
+      currentAmount: 90, 
+      allocatedAmount: 200, 
+      percentageUsed: 40 
+    },
+    { 
+      name: 'Entertainment', 
+      currentAmount: 120, 
+      allocatedAmount: 150, 
+      percentageUsed: 80 
+    },
+  ];
+
+  getStaticExpenses(): Expenses[] {
+    return [...this.STATIC_EXPENSES];
+  }
+}
