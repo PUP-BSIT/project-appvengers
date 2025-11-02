99df77a69462dcea14173bbe04d0e81ac420ebbe
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-11-02 13:37:29 +0800

    fix(income): rename user_model to user.model

---
+++ frontend/ibudget/src/services/income.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Income } from '../app/model/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IncomeService {
+  private readonly STATIC_INCOME: Income[] = [
+    {
+      name: 'Salary',
+      currentAmount: 2250, 
+      amount: 3000, 
+      percentageCompleted: 75
+    },
+    {
+      name: 'Freelance', 
+      currentAmount: 400,
+      amount: 800, 
+      percentageCompleted: 50
+    },
+    {
+      name: 'Others', 
+      currentAmount: 49,
+      amount: 200, 
+      percentageCompleted: 20
+    }
+  ];
+
+  getStaticIncome(): Income[] {
+    return [...this.STATIC_INCOME];
+  }
+}
