c96a08a890c13257c31d60347bbf3d036e37e764
delima-justine <delimajustine24@gmail.com>
2025-11-03 18:49:34 +0800

    feat(budgets): add key performance indicator component.

---
+++ frontend/ibudget/src/app/budgets/kpi-panel/kpi-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kpi-panel',
+  imports: [],
+  templateUrl: './kpi-panel.html',
+  styleUrl: './kpi-panel.scss',
+})
+export class KpiPanel {
+
+}
