5bd64ff47629bf4ae7d8fa005b3dc3170df3d4df
delima-justine <delimajustine24@gmail.com>
2025-11-03 21:54:56 +0800

    changes(kpi-panel): update kpi panel.

--- frontend/ibudget/src/app/budgets/kpi-panel/kpi-panel.ts
+++ frontend/ibudget/src/app/budgets/kpi-panel/kpi-panel.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RoundProgressComponent } from 'angular-svg-round-progressbar';
 
 @Component({
   selector: 'app-kpi-panel',
-  imports: [],
+  imports: [RoundProgressComponent],
   templateUrl: './kpi-panel.html',
   styleUrl: './kpi-panel.scss',
 })
 export class KpiPanel {
-
+  
 }
