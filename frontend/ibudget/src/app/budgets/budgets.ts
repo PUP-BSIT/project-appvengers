8eb4850386a6a136c88f8b0529110387469a70d0
delima-justine <delimajustine24@gmail.com>
2025-11-04 12:13:07 +0800

    feat(budgets): add new key performance indicator(kpi) panel.

--- frontend/ibudget/src/app/budgets/budgets.ts
+++ frontend/ibudget/src/app/budgets/budgets.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
+import { KpiPanel } from "./kpi-panel/kpi-panel";
 
 @Component({
   selector: 'app-budgets',
-  imports: [Sidebar, Header],
+  imports: [Sidebar, Header, KpiPanel],
   templateUrl: './budgets.html',
   styleUrl: './budgets.scss',
 })
 export class Budgets {
-
+  
 }
