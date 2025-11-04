e3c33ff1ceee4be5c85b3fa7a1a19a7279eca244
delima-justine <delimajustine24@gmail.com>
2025-11-04 12:26:16 +0800

    feat(budgets): add budget-list component.

--- frontend/ibudget/src/app/budgets/budgets.ts
+++ frontend/ibudget/src/app/budgets/budgets.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
 import { KpiPanel } from "./kpi-panel/kpi-panel";
+import { BudgetList } from "./budget-list/budget-list";
 
 @Component({
   selector: 'app-budgets',
-  imports: [Sidebar, Header, KpiPanel],
+  imports: [Sidebar, Header, KpiPanel, BudgetList],
   templateUrl: './budgets.html',
   styleUrl: './budgets.scss',
 })
 export class Budgets {
-  
+
 }
