38f0fe62689e1a7c8108fb6b8b0defdffa67e282
delima-justine <delimajustine24@gmail.com>
2025-11-04 11:39:16 +0800

    clean(budgets): remove kpi panel and budget-panel.

--- frontend/ibudget/src/app/budgets/budgets.ts
+++ frontend/ibudget/src/app/budgets/budgets.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
-import { BudgetsPanel } from "./budgets-panel/budgets-panel";
 
 @Component({
   selector: 'app-budgets',
-  imports: [Sidebar, Header, BudgetsPanel],
+  imports: [Sidebar, Header],
   templateUrl: './budgets.html',
   styleUrl: './budgets.scss',
 })
