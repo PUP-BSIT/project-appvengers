df55d116d30a294c583705c596a6d15169eab987
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-11-13 22:24:08 +0800

    fix(dashboard): remove hardcoded username and budget values
    Implemented dynamic fetching of username and remaining budget. It will default to 'User' and 0 if the user uses the app for the first time

--- frontend/ibudget/src/app/dashboard/dashboard.ts
+++ frontend/ibudget/src/app/dashboard/dashboard.ts
@@ -4,13 +4,38 @@ import { ExpenseChart } from './expense-chart/expense-chart';
 import { HistoryTable } from "./history-table/history-table";
 import { Header } from "../header/header";
 import { IncomeChart } from "./income-chart/income-chart";
+import { DecimalPipe } from '@angular/common';
+import { HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Sidebar, HistoryTable, Header, IncomeChart, ExpenseChart],
+  imports: [
+    Sidebar, 
+    HistoryTable, 
+    Header, 
+    IncomeChart, 
+    ExpenseChart, 
+    DecimalPipe
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
-  
+  username: string = '';
+  remainingBudget: number = 0;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('api/user/profile').subscribe({
+      next: (res) => {
+        this.username = res.username;
+        this.remainingBudget = res.remainingBudget;
+      },
+      error: () => {
+        this.username = 'User';
+        this.remainingBudget = 0;
+      }
+    });
+  }
 }
