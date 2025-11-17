49a5ba7024b8b88ede6f046bc2730043b8c743a9
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2025-11-17 23:58:05 +0800

    docs(devops): add nginx config and update n8n webhook endpoint

--- frontend/ibudget/src/environments/environment.ts
+++ frontend/ibudget/src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8081/api',
-  n8nWebhookUrl: 'https://n8n-j3he.onrender.com/webhook/00a671bf-4606-4030-a2a2-65aa9e3b6c44/chat'
+  n8nWebhookUrl: 'https://n8n-j3he.onrender.com/webhook/5ecf1a6a-50e1-4b3f-8df3-42db0d105e8c/chat'
 };
