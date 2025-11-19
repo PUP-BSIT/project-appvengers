a8d01463f9a560c3a00852936c5340fbc6e46d75
delima-justine <delimajustine24@gmail.com>
2025-11-19 21:36:54 +0800

    feat(savings): implement AddSaving component with form and routing

--- frontend/ibudget/src/app/savings/add-saving/add-saving.ts
+++ frontend/ibudget/src/app/savings/add-saving/add-saving.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Sidebar } from "../../sidebar/sidebar";
+import { Header } from "../../header/header";
 
 @Component({
   selector: 'app-add-saving',
-  imports: [],
+  imports: [Sidebar, Header],
   templateUrl: './add-saving.html',
   styleUrl: './add-saving.scss',
 })
