9e63fee86539635cb83c4521d7ac484651dc3ab2
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2025-11-20 04:53:38 +0800

    fix: update n8n webhook url to latest version

--- frontend/ibudget/src/environments/environment.ts
+++ frontend/ibudget/src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8081/api',
-  n8nWebhookUrl: 'https://n8n-j3he.onrender.com/webhook/5ecf1a6a-50e1-4b3f-8df3-42db0d105e8c/chat'
+  n8nWebhookUrl: 'https://n8n-j3he.onrender.com/webhook/90ac43cb-ae07-42ee-9532-36b9940f7cd3/chat'
 };
