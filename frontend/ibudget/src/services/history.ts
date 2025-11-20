60337ed440a40635a33f543340433a7a58f967aa
delima-justine <delimajustine24@gmail.com>
2025-11-20 19:54:58 +0800

    fix(history): update transaction model to use SavingTransaction and remove unused fields

--- frontend/ibudget/src/services/history.ts
+++ frontend/ibudget/src/services/history.ts
@@ -1,44 +1,51 @@
 import { Injectable } from '@angular/core';
-import { TransactionHistory } from '../models/user.model';
+import { SavingTransaction } from '../models/user.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HistoryService {
-  private readonly STATIC_HISTORY: TransactionHistory[] = [
+  private readonly STATIC_HISTORY: SavingTransaction[] = [
     {
+      id: 1,
+      user_id: 1,
+      transaction_date: '2024-05-20',
+      savings_action: 'Deposit',
+      date: new Date('2025-05-15'),
+      description: 'Remaining Allowance',
       amount: 300,
-      category: 'Entertainment',
-      type: 'Expenses'
     },
     {
-      amount: 5000,
-      category: 'Salary',
-      type: 'Income'
+      id: 2,
+      user_id: 1,
+      transaction_date: '2024-05-20',
+      savings_action: 'Deposit',
+      date: new Date('2025-05-16'),
+      description: 'Remaining Allowance',
+      amount: 100,
     },
     {
-      amount: 6000,
-      category: 'Investments',
-      type: 'Income'
+      id: 3,
+      user_id: 1,
+      transaction_date: '2025-05-21',
+      savings_action: 'Deposit',
+      date: new Date('2024-05-16'),
+      description: 'Remaining Allowance',
+      amount: 100,
     },
     {
-      amount: 500,
-      category: 'Food',
-      type: 'Expenses'
+      id: 4,
+      user_id: 1,
+      transaction_date: '2024-05-22',
+      savings_action: 'Withdrawal',
+      date: new Date('2024-05-16'),
+      description: 'Remaining Allowance',
+      amount: 100,
     },
-    {
-      amount: 1700,
-      category: 'Utilities',
-      type: 'Expenses'
-    },
-    {
-      amount: 1000,
-      category: 'Expenses',
-      type: 'Others'
-    }
   ];
   
-  getStaticHistory(): TransactionHistory[] {
-    return [...this.STATIC_HISTORY];
+  getStaticHistory(): Observable<SavingTransaction[]> {
+    return of(this.STATIC_HISTORY);
   }
 }
