3cc176feac794f112cceb8f21f9419b4cfde5104
delima-justine <delimajustine24@gmail.com>
2025-11-20 19:55:38 +0800

    feat(add-saving): add router link to save button for navigation

--- frontend/ibudget/src/app/savings/add-saving/add-saving.ts
+++ frontend/ibudget/src/app/savings/add-saving/add-saving.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../../sidebar/sidebar";
 import { Header } from "../../header/header";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-saving',
-  imports: [Sidebar, Header],
+  imports: [Sidebar, Header, RouterLink],
   templateUrl: './add-saving.html',
   styleUrl: './add-saving.scss',
 })
