9e185e67d4be5aa4bc6daec6517876464f4ab445
delima-justine <delimajustine24@gmail.com>
2025-11-20 23:48:19 +0800

    feat(view-saving): add delete functionality and update router integration

--- frontend/ibudget/src/app/savings/view-saving/view-saving.ts
+++ frontend/ibudget/src/app/savings/view-saving/view-saving.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Sidebar } from "../../sidebar/sidebar";
 import { Header } from "../../header/header";
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { HistoryService } from '../../../services/history';
 import { Saving, SavingTransaction } from '../../../models/user.model';
 import { SavingsService } from '../../../services/savings.service';
@@ -19,8 +19,10 @@ export class ViewSaving implements OnInit{
   historyService = inject(HistoryService);
   savingService = inject(SavingsService);
   activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
   savingId = signal(0);
 
+  // Initialize component and fetch data
   ngOnInit(): void {
     this.getSavingsTransactionHistories();
     const savingsId = this.activatedRoute.snapshot.paramMap.get('id') ?? '';
@@ -29,21 +31,31 @@ export class ViewSaving implements OnInit{
     this.getSavingsData();
   }
 
+  // Fetch all transaction histories
   getSavingsTransactionHistories() {
     this.historyService.getStaticHistory().subscribe((historiesData) => {
       this.transactionHistories.set(historiesData);
     });
   }
 
+  // Filter transactions related to the current saving
   filterTransactions() {
     const filtered = this.transactionHistories()
       .filter((transaction) => transaction.id === this.savingId());
     this.filteredTransactions.set(filtered);
   }
 
+  // Fetch current saving data
   getSavingsData() {
     this.savingService.getSavingById(this.savingId()).subscribe((savingData) => {
       this.currentSaving.set(savingData);
+    });
+  }
+
+  // Delete the current view saving
+  deleteSaving() {
+    this.savingService.deleteSaving(this.savingId()).subscribe(() => {
+      this.router.navigate(['/savings']);
     });
   }
 }
