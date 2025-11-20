27aaa9d09a91942957a40fec10e4b6a1b37c7ae0
delima-justine <delimajustine24@gmail.com>
2025-11-20 19:55:23 +0800

    feat(view-saving): implement transaction history retrieval and filtering logic

--- frontend/ibudget/src/app/savings/view-saving/view-saving.ts
+++ frontend/ibudget/src/app/savings/view-saving/view-saving.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Sidebar } from "../../sidebar/sidebar";
 import { Header } from "../../header/header";
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { HistoryService } from '../../../services/history';
+import { Saving, SavingTransaction } from '../../../models/user.model';
 
 @Component({
   selector: 'app-view-saving',
-  imports: [Sidebar, Header],
+  imports: [Sidebar, Header, RouterLink],
   templateUrl: './view-saving.html',
   styleUrls: ['./view-saving.scss'],
 })
-export class ViewSaving {
+export class ViewSaving implements OnInit{
+  transactionHistories = signal(<SavingTransaction[]>[]);
+  filteredTransactions = signal(<SavingTransaction[]>[]);
+  historyService = inject(HistoryService);
+  activatedRoute = inject(ActivatedRoute);
+  savingId = signal(0);
 
+  ngOnInit(): void {
+    this.getSavingsTransactionHistories();
+    const savingsId = this.activatedRoute.snapshot.paramMap.get('id') ?? '';
+    this.savingId.set(+savingsId);
+    this.filterTransactions();
+  }
+
+  getSavingsTransactionHistories() {
+    this.historyService.getStaticHistory().subscribe((historiesData) => {
+      this.transactionHistories.set(historiesData);
+    });
+  }
+
+  filterTransactions() {
+    const filtered = this.transactionHistories()
+      .filter((transaction) => transaction.id === this.savingId());
+    this.filteredTransactions.set(filtered);
+  }
 }
