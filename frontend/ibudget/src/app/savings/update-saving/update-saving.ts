fa7a92925f0c816b4e58a0624bf8ff64aeb47320
delima-justine <delimajustine24@gmail.com>
2025-11-20 19:55:54 +0800

    feat(update-saving): add router links for update and cancel actions

--- frontend/ibudget/src/app/savings/update-saving/update-saving.ts
+++ frontend/ibudget/src/app/savings/update-saving/update-saving.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "../../header/header";
 import { Sidebar } from "../../sidebar/sidebar";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-update-saving',
-  imports: [Header, Sidebar],
+  imports: [Header, Sidebar, RouterLink],
   templateUrl: './update-saving.html',
   styleUrl: './update-saving.scss',
 })
