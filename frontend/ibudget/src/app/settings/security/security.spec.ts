e894771f11c116fc8a9c1612cae4fe9c559d20c4
delima-justine <delimajustine24@gmail.com>
2025-11-21 15:45:12 +0800

    feat(tests): add HTTP client and route provider to account, security, and sub-header tests

--- frontend/ibudget/src/app/settings/security/security.spec.ts
+++ frontend/ibudget/src/app/settings/security/security.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { Security } from './security';
 
 describe('Security', () => {
@@ -8,7 +9,19 @@ describe('Security', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Security]
+      imports: [Security],
+      providers: [
+        provideHttpClient(),
+        {provide: ActivatedRoute, 
+          useValue: {
+            snapshot: { 
+              paramMap: convertToParamMap(
+                {id: '1'}
+              ) 
+            } 
+          }
+        }
+      ]
     })
     .compileComponents();
 
