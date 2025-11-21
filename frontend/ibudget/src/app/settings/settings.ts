59273eb30ee1a6ae6e9d79978e787034da227cfe
delima-justine <delimajustine24@gmail.com>
2025-11-21 15:03:07 +0800

    refactor(settings): simplify settings component by removing unused form and related elements

--- frontend/ibudget/src/app/settings/settings.ts
+++ frontend/ibudget/src/app/settings/settings.ts
@@ -2,55 +2,14 @@ import { Component, inject, signal } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
 import { SubHeader } from './sub-header/sub-header';
-import { ViewportScroller } from '@angular/common';
-import { 
-    FormGroup, 
-    FormControl,
-    ReactiveFormsModule, 
-    FormBuilder,
-    Validators
-  } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
-  imports: [Sidebar, Header, SubHeader, ReactiveFormsModule],
+  imports: [Sidebar, Header, SubHeader, RouterOutlet],
   templateUrl: './settings.html',
   styleUrl: './settings.scss',
 })
 export class Settings {
-  securitySettingsForm: FormGroup;
-  formBuilder = inject(FormBuilder);
-  hideCurrentPassword = signal(true);
-  hideNewPassword = signal(true);
-  hideConfirmPassword = signal(true);
 
-  constructor(private viewportScroller: ViewportScroller) {
-    this.securitySettingsForm = this.formBuilder.group({
-      current_password: ['', {
-        validators: [Validators.required]
-      }],
-      new_password: ['', {
-        validators: [Validators.minLength(12)]
-      }],
-      confirm_password: ['', {
-        validators: [Validators.required]
-      }]
-    });
-  }
-
-  get currentPassword() {
-    return this.securitySettingsForm.get('current_password');
-  }
-
-  get newPassword() {
-    return this.securitySettingsForm.get('new_password');
-  }
-
-  get confirmPassword() {
-    return this.securitySettingsForm.get('confirm_password');
-  }
-
-  scrollToPanel(anchorId: string) {
-    this.viewportScroller.scrollToAnchor(anchorId);
-  }
 }
