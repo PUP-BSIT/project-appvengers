63c23f52c835b043f958b9baad96ed0758e5dfbd
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-11-26 19:26:49 +0800

    fix(transaction controller): change endpoint path

--- backend/appvengers/src/main/java/com/backend/appvengers/controller/TransactionController.java
+++ backend/appvengers/src/main/java/com/backend/appvengers/controller/TransactionController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/transactions")
+@RequestMapping("/api/expenses")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
 public class TransactionController {
