73a815709d10d22f4e9b8ebc1310a4f6fda19d61
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-11-26 19:12:53 +0800

    feat(backend): add a query in transaction repository to get the expense summary by category

--- backend/appvengers/src/main/java/com/backend/appvengers/repository/TransactionRepository.java
+++ backend/appvengers/src/main/java/com/backend/appvengers/repository/TransactionRepository.java
@@ -3,11 +3,23 @@ package com.backend.appvengers.repository;
 import com.backend.appvengers.entity.Transaction;
 import com.backend.appvengers.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
     List<Transaction> findByUser(User user);
+
+    @Query("""
+        SELECT COALESCE(c.name, 'Uncategorized'), SUM(t.amount)
+        FROM Transaction t
+        LEFT JOIN t.category c
+        WHERE t.isDeleted = false AND t.user = :user
+        GROUP BY COALESCE(c.name, 'Uncategorized')
+    """)
+    List<Object[]> findExpenseSummaryByUser(@Param("user") User user);
 }
