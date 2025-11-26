b4cf003dccc28a08439ef8bf345a2c778ce97c33
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-11-27 06:38:29 +0800

    test(auth-page): add HttpClientTestingModule and RouterTestingModule to test imports

--- frontend/ibudget/src/app/auth-page/auth-page.spec.ts
+++ frontend/ibudget/src/app/auth-page/auth-page.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthPage } from './auth-page';
 
@@ -8,7 +10,7 @@ describe('AuthPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AuthPage]
+      imports: [AuthPage, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
 
