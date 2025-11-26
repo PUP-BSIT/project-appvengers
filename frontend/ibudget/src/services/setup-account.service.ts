e10be5da75db192b7cdb57f069e7b64729d0f0f4
delima-justine <delimajustine24@gmail.com>
2025-11-26 13:35:55 +0800

    feat(setup-account): add SetupAccountService with account setup token verification functionality

---
+++ frontend/ibudget/src/services/setup-account.service.ts
@@ -0,0 +1,27 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { AccountSetupResponse, ApiResponse } from '../models/user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SetupAccountService {
+  private apiUrl = `${environment.apiUrl}/auth`;
+  http = inject(HttpClient);
+
+  verifyAccountSetupToken(
+    token: string,
+    username: string,
+    accountSetupResponse: AccountSetupResponse
+  ): Observable<ApiResponse> {
+    const url =
+      `${this.apiUrl}/verify-account-setup?token=${encodeURIComponent(token)}&username=${encodeURIComponent(username)}`;
+
+    // body is the User-like object expected by the backend
+    return this.http.put<ApiResponse>(url, accountSetupResponse, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    });
+  }
+}
