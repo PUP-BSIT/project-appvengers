1d28f5efc13c75049fdc36bb84a23b336f82a004
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-11-28 22:33:17 +0800

    refactor(transaction-repository): create separate query for expense and summary

--- backend/appvengers/src/main/java/com/backend/appvengers/repository/TransactionRepository.java
+++ backend/appvengers/src/main/java/com/backend/appvengers/repository/TransactionRepository.java
@@ -1,5 +1,5 @@
 package com.backend.appvengers.repository;
-
+import java.util.List;
 import com.backend.appvengers.entity.Transaction;
 import com.backend.appvengers.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,20 +7,27 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByUserIdAndTypeAndDeletedFalse(Long userId, String type);
     List<Transaction> findByUser(User user);
 
+    // Expense summary
     @Query("""
         SELECT COALESCE(t.category, 'Uncategorized'), SUM(t.amount)
         FROM Transaction t
-        WHERE t.user = :user AND t.type = :type AND t.deleted = false
+        WHERE t.user = :user AND t.type = 'EXPENSE' AND t.deleted = false
         GROUP BY COALESCE(t.category, 'Uncategorized')
     """)
-    
-    List<Object[]> findSummaryByUserAndType(@Param("user") User user, @Param("type") String type);
+    List<Object[]> findExpenseSummaryByUser(@Param("user") User user);
+
+    // Income summary
+    @Query("""
+        SELECT COALESCE(t.category, 'Uncategorized'), SUM(t.amount)
+        FROM Transaction t
+        WHERE t.user = :user AND t.type = 'INCOME' AND t.deleted = false
+        GROUP BY COALESCE(t.category, 'Uncategorized')
+    """)
+    List<Object[]> findIncomeSummaryByUser(@Param("user") User user);
 }
