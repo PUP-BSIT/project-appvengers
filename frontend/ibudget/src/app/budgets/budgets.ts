6eaf5d870bde83981072c68168782d85ee4fa255
delima-justine <delimajustine24@gmail.com>
2025-11-28 23:19:15 +0800

    fix(budgets): remove unused KpiPanel import from budgets component

--- frontend/ibudget/src/app/budgets/budgets.ts
+++ frontend/ibudget/src/app/budgets/budgets.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
-import { KpiPanel } from "./kpi-panel/kpi-panel";
 import { BudgetList } from "./budget-list/budget-list";
 
 @Component({
   selector: 'app-budgets',
-  imports: [Sidebar, Header, KpiPanel, BudgetList],
+  imports: [Sidebar, Header, BudgetList],
   templateUrl: './budgets.html',
   styleUrl: './budgets.scss',
 })
