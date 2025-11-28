af3a5948f885adf9c63283579bb3a18de07b8bae
delima-justine <delimajustine24@gmail.com>
2025-11-28 16:50:40 +0800

    refactor(app): simplify HTTP client configuration and remove unused imports

--- frontend/ibudget/src/app/app.config.ts
+++ frontend/ibudget/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideLoadingBarRouter } from '@ngx-loading-bar/router'
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideLoadingBarInterceptor } from '@ngx-loading-bar/http-client';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { authInterceptor } from '../services/auth.interceptor';
 import { 
         ApplicationConfig, 
@@ -21,9 +21,11 @@ export const appConfig: ApplicationConfig = {
         scrollPositionRestoration: 'top',
       })
     ),
-    provideHttpClient(),
     provideCharts(withDefaultRegisterables()),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(
+      withInterceptors([authInterceptor]),
+      withInterceptorsFromDi()
+    ),
     provideLoadingBarInterceptor(),
     provideLoadingBarRouter()
   ]
