6c24552f17df82458e6a07ebb046d9fbcb673a05
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-11-28 22:55:35 +0800

    fix(income-summary): change income summary to a record

--- backend/appvengers/src/main/java/com/backend/appvengers/dto/IncomeSummary.java
+++ backend/appvengers/src/main/java/com/backend/appvengers/dto/IncomeSummary.java
@@ -1,20 +1,5 @@
 package com.backend.appvengers.dto;
+
 import java.util.List;
 
-public class IncomeSummary {
-    private List<String> labels;
-    private List<Double> values;
-
-    public IncomeSummary(List<String> labels, List<Double> values) {
-        this.labels = labels;
-        this.values = values;
-    }
-
-    public List<String> getLabels() {
-        return labels;
-    }
-
-    public List<Double> getValues() {
-        return values;
-    }
-}
+public record IncomeSummary(List<String> labels, List<Double> values) {}
