e562257e086fcfd29216098de08f895a105e597c
delima-justine <delimajustine24@gmail.com>
2025-12-02 00:39:17 +0800

    fix(categories): correct category names for consistency in expense categories

--- frontend/ibudget/src/services/categories.service.ts
+++ frontend/ibudget/src/services/categories.service.ts
@@ -11,9 +11,9 @@ export class CategoriesService {
   // Mock Expense Categories
   EXPENSE_CATEGORIES: Categories[] = [
     { category_id: 1, name: 'Housing', type: 'expense' },
-    { category_id: 2, name: 'Transportation', type: 'expense' },
+    { category_id: 2, name: 'Transport', type: 'expense' },
     { category_id: 3, name: 'Utilities', type: 'expense' },
-    { category_id: 4, name: 'Insurance', type: 'expense' },
+    { category_id: 4, name: 'Entertainment', type: 'expense' },
     { category_id: 5, name: 'Healthcare', type: 'expense' },
     { category_id: 6, name: 'Food', type: 'expense' },
     { category_id: 7, name: 'Other', type: 'expense' }
