f42938f911f13f74e4bd98b5f3aa4750cc1fa36a
delima-justine <delimajustine24@gmail.com>
2025-12-02 15:24:41 +0800

    test(add-budget-button.spec): enhance test setup with HTTP client providers

--- frontend/ibudget/src/app/budgets/budget-list/add-budget-button/add-budget-button.spec.ts
+++ frontend/ibudget/src/app/budgets/budget-list/add-budget-button/add-budget-button.spec.ts
@@ -1,23 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { AddBudgetButton } from './add-budget-button';
 
 describe('AddBudgetButton', () => {
-  let component: AddBudgetButton;
   let fixture: ComponentFixture<AddBudgetButton>;
+  let component: AddBudgetButton;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AddBudgetButton]
-    })
-    .compileComponents();
+      imports: [AddBudgetButton],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting() 
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddBudgetButton);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should create', () => expect(component).toBeTruthy());
 });
