4fa17931283777aeab1d8de2480f7f43b6ca8f70
delima-justine <delimajustine24@gmail.com>
2025-12-03 02:24:53 +0800

    feat(sub-header): dynamically set user ID in account settings link

--- frontend/ibudget/src/app/settings/sub-header/sub-header.ts
+++ frontend/ibudget/src/app/settings/sub-header/sub-header.ts
@@ -1,5 +1,6 @@
-import { Component, inject, output } from '@angular/core';
+import { Component, inject, OnInit, output, signal } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-sub-header',
@@ -7,6 +8,21 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './sub-header.html',
   styleUrl: './sub-header.scss',
 })
-export class SubHeader {
+export class SubHeader implements OnInit{
+  userId = signal(0);
   router = inject(Router);
+  authService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.getUserId();
+    console.log(this.userId());
+  }
+
+  getUserId() {
+    this.authService.getProfile().subscribe((res) => {
+      if (res.success && res.data) {
+        this.userId.set(res.data.id);
+      }
+    })
+  }
 }
