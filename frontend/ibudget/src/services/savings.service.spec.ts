2ac95993a5d64a76e50a78fc9e67ca280731b510
delima-justine <delimajustine24@gmail.com>
2025-12-03 16:50:08 +0800

    feat(savings.service): add HTTP client providers for testing SavingsService

--- frontend/ibudget/src/services/savings.service.spec.ts
+++ frontend/ibudget/src/services/savings.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { SavingsService } from './savings.service';
 
@@ -6,7 +8,12 @@ describe('SavingsService', () => {
   let service: SavingsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(SavingsService);
   });
 
