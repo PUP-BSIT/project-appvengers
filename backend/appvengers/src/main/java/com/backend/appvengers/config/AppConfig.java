13bfc7f9bfe3de9a62da8b39e8addafc2df91044
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-12-06 14:52:13 +0800

    refactor(app config): comment out cors configuration for future reference

--- backend/appvengers/src/main/java/com/backend/appvengers/config/AppConfig.java
+++ backend/appvengers/src/main/java/com/backend/appvengers/config/AppConfig.java
@@ -1,18 +1,18 @@
-package com.backend.appvengers.config;
+// package com.backend.appvengers.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class AppConfig implements WebMvcConfigurer {
+// @Configuration
+// public class AppConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200", "https://i-budget.site", "https://www.i-budget.site")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/api/**")
+//                 .allowedOrigins("http://localhost:4200", "https://i-budget.site", "https://www.i-budget.site")
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                 .allowedHeaders("*")
+//                 .allowCredentials(true);
+//     }
+// }
