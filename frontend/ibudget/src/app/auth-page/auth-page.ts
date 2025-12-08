7be2e7cf0b78ecebc75780d22ddc58ffaf5cfedd
delima-justine <delimajustine24@gmail.com>
2025-12-09 05:39:43 +0800

    style: update auth page layout and enhance responsiveness with new app name class

--- frontend/ibudget/src/app/auth-page/auth-page.ts
+++ frontend/ibudget/src/app/auth-page/auth-page.ts
@@ -2,10 +2,11 @@ import { Component, signal, WritableSignal } from '@angular/core';
 import { Login } from "../login/login";
 import { SignUp } from '../sign-up/sign-up';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-page',
-  imports: [Login, SignUp, CommonModule],
+  imports: [Login, SignUp, CommonModule, RouterLink],
   templateUrl: './auth-page.html',
   styleUrl: './auth-page.scss',
 })
