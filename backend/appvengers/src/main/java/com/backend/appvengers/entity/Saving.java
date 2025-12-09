06183ed0778e35a66cc8d8217a8c8af845515045
delima-justine <delimajustine24@gmail.com>
2025-12-09 21:15:31 +0800

    fix(Saving): fix Saving Entity not inserting creation and update timestamp.

--- backend/appvengers/src/main/java/com/backend/appvengers/entity/Saving.java
+++ backend/appvengers/src/main/java/com/backend/appvengers/entity/Saving.java
@@ -3,8 +3,10 @@ package com.backend.appvengers.entity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -51,9 +53,11 @@ public class Saving {
   @Column(name = "description")
   private String description;
 
-  @Column(name = "created_at")
+  @CreationTimestamp
+  @Column(name = "created_at", updatable = false)
   private LocalDateTime createdAt;
 
+  @UpdateTimestamp
   @Column(name = "updated_at")
   private LocalDateTime updatedAt;
 
