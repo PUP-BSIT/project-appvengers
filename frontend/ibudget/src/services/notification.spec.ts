2f659809c129cadcf4f2e77d6f3ce8ad1373a09e
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2025-12-12 02:54:15 +0800

    fix: add HttpClient provider to NotificationService test

--- frontend/ibudget/src/services/notification.spec.ts
+++ frontend/ibudget/src/services/notification.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { NotificationService } from './notification';
 
@@ -6,7 +7,9 @@ describe('NotificationService', () => {
   let service: NotificationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()]
+    });
     service = TestBed.inject(NotificationService);
   });
 
