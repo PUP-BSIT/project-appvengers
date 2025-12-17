0072389e823082c33bde8f23d4257a00cf703059
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-12-17 20:21:09 +0800

    feat(kpi-panel): implement budget transaction service

--- frontend/ibudget/src/app/budgets/kpi-panel/kpi-panel.ts
+++ frontend/ibudget/src/app/budgets/kpi-panel/kpi-panel.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ChangeDetectorRef, signal, inject } from '@angular/core';
+import { Component, OnInit, signal, inject } from '@angular/core';
 import { RoundProgressComponent } from 'angular-svg-round-progressbar';
 import { Budget } from '../../../models/user.model';
 import { DecimalPipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { BudgetService } from '../../../services/budget.service';
+import { BudgetTransactionsService } from '../../../services/budget.transactions.service';
 
 @Component({
   selector: 'app-kpi-panel',
@@ -15,12 +16,13 @@ import { BudgetService } from '../../../services/budget.service';
 export class KpiPanel implements OnInit {
   remainingBudget: number = 0;
   totalBudget: number = 0;
-  totalExpenses: number = 700;
+  totalExpenses: number = 0;
   budgetPercent: number = 0;
   activatedRoute = inject(ActivatedRoute);
   budgetService = inject(BudgetService);
   currentBudget = signal<Budget>({} as Budget);
   budgetId = signal(Number(this.activatedRoute.snapshot.paramMap.get('id') ?? 1));
+  budgetTransactionsService = inject(BudgetTransactionsService);
 
   ngOnInit(): void {
     this.getBudgetData();
@@ -32,22 +34,24 @@ export class KpiPanel implements OnInit {
 
   // Fetch budget data based on budgetId (budget service)
   getBudgetData() {
-    const id = this.budgetId();
-    this.budgetService.getBudgetById(id)
-      .subscribe({
-        next: (budget) => {
-          this.currentBudget.set(budget);
-          this.totalBudget = budget.limit_amount ?? 0;
-          this.totalExpenses = budget.current_amount ?? 0;
-          this.remainingBudget = Math.max(
-            this.totalBudget - this.totalExpenses, 0
-          );
-          this.budgetPercent = this.totalBudget > 0
-            ? Math.round((this.totalExpenses / this.totalBudget) * 100)
-            : 0;
-        },
-        error: (err) => console.error(
-          'Failed to load budget for KPI panel', err)
-      });
+  const id = this.budgetId();
+
+  this.budgetTransactionsService.getBudgetSummary(id)
+    .subscribe({
+      next: (summary) => {
+        this.totalBudget = summary.limitAmount ?? 0;
+        this.totalExpenses = summary.totalExpenses ?? 0;
+        this.remainingBudget = summary.remainingBudget ?? 0;
+
+        this.budgetPercent = this.totalBudget > 0
+          ? Math.round((this.totalExpenses / this.totalBudget) * 100)
+          : 0;
+
+        this.currentBudget.set({
+          category_name: summary.categoryName
+        } as any);
+      },
+      error: (err) => console.error('Failed to load budget summary', err)
+    });
   }
 }
