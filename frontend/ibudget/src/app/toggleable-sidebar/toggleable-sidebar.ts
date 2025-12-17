1cdc9e0a13ff137a39fe72fb7bb98fa9651b3272
delima-justine <delimajustine24@gmail.com>
2025-12-17 18:46:13 +0800

    feat(toggleable-sidebar): implement toggle functionality and update styles for expanded state

--- frontend/ibudget/src/app/toggleable-sidebar/toggleable-sidebar.ts
+++ frontend/ibudget/src/app/toggleable-sidebar/toggleable-sidebar.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-toggleable-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './toggleable-sidebar.html',
   styleUrl: './toggleable-sidebar.scss',
 })
 export class ToggleableSidebar {
+  isExpanded = false;
 
+  toggle(): void {
+    this.isExpanded = !this.isExpanded;
+  }
 }
