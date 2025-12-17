fc6b55b02f8b00c6a29d82fa2cc8dc125bac57b6
delima-justine <delimajustine24@gmail.com>
2025-12-17 21:54:27 +0800

    feat(toggleable-sidebar): enhance sidebar functionality with dynamic type handling and improved styles

--- frontend/ibudget/src/app/toggleable-sidebar/toggleable-sidebar.ts
+++ frontend/ibudget/src/app/toggleable-sidebar/toggleable-sidebar.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgClass } from '@angular/common';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-toggleable-sidebar',
@@ -10,8 +11,23 @@ import { NgClass } from '@angular/common';
 })
 export class ToggleableSidebar {
   isExpanded = false;
+  isSidebarControlOpen = signal(false);
+  sidebarService = inject(SidebarService);
 
   toggle(): void {
     this.isExpanded = !this.isExpanded;
+  }
+
+  toggleSidebarControl(): void {
+    this.isSidebarControlOpen.set(!this.isSidebarControlOpen());
+  }
+
+  setSidebarType(type: string): void {
+    this.sidebarService.setSidebarType(type);
+    this.isSidebarControlOpen.set(false);
+  }
+
+  getSidebarType(): string {
+    return this.sidebarService.getSidebarType();
   }
 }
