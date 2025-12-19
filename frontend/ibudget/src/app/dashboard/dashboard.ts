c8db7688f4705a0973f045c48742e174c0331563
delima-justine <delimajustine24@gmail.com>
2025-12-19 20:06:18 +0800

    feat(dashboard): enhance user onboarding with greeting and guidance panels

--- frontend/ibudget/src/app/dashboard/dashboard.ts
+++ frontend/ibudget/src/app/dashboard/dashboard.ts
@@ -1,80 +1,19 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { Sidebar } from '../sidebar/sidebar';
-import { ExpenseChart } from './expense-chart/expense-chart';
+import { Component } from '@angular/core';
 import { Header } from '../header/header';
-import { IncomeChart } from './income-chart/income-chart';
-import { DecimalPipe } from '@angular/common';
-import { AuthService } from '../../services/auth.service';
-import { SpendingChart } from './spending-chart/spending-chart';
-import { Router } from '@angular/router';
-import { Transaction } from '../../models/user.model';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
 import { ToggleableSidebar } from "../toggleable-sidebar/toggleable-sidebar";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     Header,
-    IncomeChart,
-    ExpenseChart,
-    DecimalPipe,
-    SpendingChart,
-    ToggleableSidebar
-],
+    ToggleableSidebar,
+    RouterLink
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 
-export class Dashboard implements OnInit {
-  username: string | null = null;
-  remainingBudget: number = 0;
-
-  constructor(
-    public auth: AuthService,
-    private http: HttpClient,
-    private cd: ChangeDetectorRef,
-    private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    this.auth.getProfile().subscribe({
-      next: (res) => {
-        if (res.success && res.data) {
-          this.username = res.data.username;
-          this.cd.detectChanges();
-        }
-      },
-      error: () => {
-        this.auth.logout();
-        this.router.navigate(['/login-page']);
-      }
-    });
-
-    const token = this.auth.getToken();
-    const url = `${environment.apiUrl}/transactions`;
-    const headers = { Authorization: `Bearer ${token}` };
-
-    this.http.get<{ success: boolean; data: Transaction[] }>(url, {
-      headers
-    }).subscribe({
-      next: (res) => {
-        const txs = res.data;
-
-        const income = txs
-          .filter(tx => tx.type === 'income')
-          .reduce((sum, tx) => sum + tx.amount, 0);
-
-        const expenses = txs
-          .filter(tx => tx.type === 'expense')
-          .reduce((sum, tx) => sum + tx.amount, 0);
-
-        this.remainingBudget = income - expenses;
-        this.cd.detectChanges();
-      },
-      error: () => {
-        console.error('Failed to load transactions for balance');
-      }
-    });
-  }
+export class Dashboard {
+ 
 }
