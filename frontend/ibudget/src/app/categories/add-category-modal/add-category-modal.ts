cc68e47d9d21e8f2f182201d40769ff346eb0cb6
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-12-23 18:44:08 +0800

    feat(add-category-modal): add connection between categories panel and add category modal

--- frontend/ibudget/src/app/categories/add-category-modal/add-category-modal.ts
+++ frontend/ibudget/src/app/categories/add-category-modal/add-category-modal.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-category-modal',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './add-category-modal.html',
   styleUrl: './add-category-modal.scss',
 })
-export class AddCategoryModal {
 
+export class AddCategoryModal {
+  categoryName = '';
+
+  saveCategory() {
+    console.log('Saving category:', this.categoryName);
+    this.categoryName = '';
+  }
 }
