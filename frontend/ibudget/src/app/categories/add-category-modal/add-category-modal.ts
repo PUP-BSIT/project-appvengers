a74ff306e8cf78efa00891e35505018c82af38b4
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-12-23 18:19:31 +0800

    feat: create "add-category-modal" for category CRUD UI

---
+++ frontend/ibudget/src/app/categories/add-category-modal/add-category-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-category-modal',
+  imports: [],
+  templateUrl: './add-category-modal.html',
+  styleUrl: './add-category-modal.scss',
+})
+export class AddCategoryModal {
+
+}
