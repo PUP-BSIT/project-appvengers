e8aa47dc5c92a248330fd27b3ba6196006f86cae
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2025-12-26 00:16:57 +0800

    hotfix: add activatedroute

--- frontend/ibudget/src/app/categories/categories-panel/categories-panel.spec.ts
+++ frontend/ibudget/src/app/categories/categories-panel/categories-panel.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Chart, registerables } from 'chart.js';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { CategoriesPanel } from './categories-panel';
 
@@ -12,7 +14,15 @@ describe('CategoriesPanel', () => {
     Chart.register(...registerables);
 
     await TestBed.configureTestingModule({
-      imports: [CategoriesPanel, HttpClientTestingModule] 
+      imports: [CategoriesPanel, HttpClientTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({})
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CategoriesPanel);
