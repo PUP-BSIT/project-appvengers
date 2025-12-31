d605134902eede0cfca18dd46d02eabffe650a44
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-12-31 09:05:52 +0800

    feat(budget-progress-bar): add dynamic color coding based on progress percentage

--- frontend/ibudget/src/app/budgets/budget-list/budget-progress-bar/budget-progress-bar.ts
+++ frontend/ibudget/src/app/budgets/budget-list/budget-progress-bar/budget-progress-bar.ts
@@ -1,11 +1,21 @@
 import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-budget-progress-bar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './budget-progress-bar.html',
   styleUrl: './budget-progress-bar.scss',
 })
+
 export class BudgetProgressBar {
- progressPercentage = input(0);
+  progressPercentage = input(0);
+
+  getBarClass() {
+    const percent = this.progressPercentage();
+    if (percent < 50) return 'bg-success';   // green
+    if (percent < 80) return 'bg-warning';   // yellow
+    return 'bg-danger';  
+  }
 }
